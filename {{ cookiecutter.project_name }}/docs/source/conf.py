c49216195cb84e67e89104624cef871c90f34a9b
Haseeb Majid <haseeb.majid@sky.uk>
2019-07-23 11:44:04 +0100

    work

--- {{ cookiecutter.project_name }}/docs/source/conf.py
+++ {{ cookiecutter.project_name }}/docs/source/conf.py
@@ -19,8 +19,8 @@ sys.path.insert(0, os.path.abspath("../../src"))
 # -- Project information -----------------------------------------------------
 
 project = "{{ cookiecutter.project_name_title }}"
-copyright = "2019, {{ cookiecutter.author_name }}"
-author = "{{ cookiecutter.author_name }}"
+copyright = "2019, {{ cookiecutter.author }}"
+author = "{{ cookiecutter.author }}"
 
 # The full version, including alpha/beta/rc tags
 release = "0.5.2"
